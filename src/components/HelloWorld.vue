11e531daabb04df10fd618fa167fdc9b8ed28730
ThomasLiew <bbsh41@qq.com>
2023-01-09 02:43:18 +0800

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    {{ msg }}
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from "vue";
+const msg = ref("Hello");
+
+onMounted(() => {
+  console.log(msg.value);
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
